e4a882e0aca9cab6e86b2205995fbf785161fc48
twigz <aharris.1990@gmail.com>
2014-12-02 22:08:29 -0500

    Setting up equipment buttons

--- Echoes/LCKCharacterViewController.m
+++ Echoes/LCKCharacterViewController.m
@@ -9,12 +9,72 @@
 #import "LCKCharacterViewController.h"
 #import "UIColor+ColorStyle.h"
 
+@interface LCKCharacterViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *helmetButton;
+@property (weak, nonatomic) IBOutlet UIButton *chestButton;
+@property (weak, nonatomic) IBOutlet UIButton *rightHandButton;
+@property (weak, nonatomic) IBOutlet UIButton *firstAccessoryButton;
+@property (weak, nonatomic) IBOutlet UIButton *secondAccessoryButton;
+@property (weak, nonatomic) IBOutlet UIButton *bootsButton;
+@property (weak, nonatomic) IBOutlet UIButton *leftHandButton;
+
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *equipmentButtons;
+
+@end
+
 @implementation LCKCharacterViewController
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor backgroundColor];
+    
+    [self setupEquipmentButtons];
+}
+
+#pragma mark - LCKCharacterViewController
+
+#pragma mark - Equipment Buttons
+
+- (void)setupEquipmentButtons {
+    for (UIButton *equipmentButton in self.equipmentButtons) {
+        equipmentButton.layer.masksToBounds = NO;
+        equipmentButton.layer.cornerRadius = 8.0;
+        equipmentButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        equipmentButton.layer.borderWidth = 1.0;
+        
+        equipmentButton.backgroundColor = [UIColor colorWithWhite:0.2 alpha:0.5];
+    }
+}
+
+- (IBAction)leftHandButtonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)rightHandbuttonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)helmetButtonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)chestButtonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)firstAccessoryButtonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)secondAccessoryButtonTapped:(UIButton *)sender {
+    
+}
+
+- (IBAction)bootsButtonTapped:(UIButton *)sender {
+    
 }
 
 @end
