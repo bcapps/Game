8a8b7c373b70f8cf5d976b9d636ccd0a0d95951d
twigz <aharris.1990@gmail.com>
2014-12-13 02:04:41 -0500

    adds the monster manual vc

---
+++ Echoes/LCKMonsterManualTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCKMonsterManualTableViewController.h
+//  Echoes
+//
+//  Created by Andrew Harrison on 12/13/14.
+//  Copyright (c) 2014 Lickability. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCKMonsterManualTableViewController : UITableViewController
+
+@end
