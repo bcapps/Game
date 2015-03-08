31d127a6bf157b7881f0249bb5cd574f6ec9e761
twigz <aharris.1990@gmail.com>
2015-03-07 19:37:06 -0500

    More enemies more items

---
+++ Echoes/LCKJournalProvider.h
@@ -0,0 +1,13 @@
+//
+//  LCKJournalProvider.h
+//  Echoes
+//
+//  Created by Andrew Harrison on 1/9/15.
+//  Copyright (c) 2015 Lickability. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LCKJournalProvider : NSObject
+
+@end
---
+++ Echoes/LCKJournalProvider.m
@@ -0,0 +1,13 @@
+//
+//  LCKJournalProvider.m
+//  Echoes
+//
+//  Created by Andrew Harrison on 1/9/15.
+//  Copyright (c) 2015 Lickability. All rights reserved.
+//
+
+#import "LCKJournalProvider.h"
+
+@implementation LCKJournalProvider
+
+@end
