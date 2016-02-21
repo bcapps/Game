8e3c77b46646048ba05445a15ce11fe26f747598
twigz <aharris.1990@gmail.com>
2016-02-21 15:59:40 -0500

    Adds JSON, decodable and object provider

---
+++ Dark Days/Dark Days/Dark Days/God.swift
@@ -0,0 +1,24 @@
+//
+//  God.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+struct God : Decodable {
+    let name : String
+    let background: String
+    let responsibilties: [String]
+    
+    static func decode(json: AnyObject) throws -> God {
+        return try God(
+            name: json => "name",
+            background: json => "background",
+            responsibilties: json => "responsibilties"
+        )
+    }
+}
---
+++ Dark Days/Dark Days/Dark Days/ObjectProvider.swift
@@ -0,0 +1,69 @@
+//
+//  ObjectProvider.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+final class ObjectProvider {
+    
+    static func objectsForJSON<T: Decodable>(JSONName: String) -> [T] {
+        let JSONArray = JSONArrayForName(JSONName)
+        
+        var objects = [T]()
+        
+        if let JSONArray = JSONArray {
+            for JSONDictionary in JSONArray {
+                if let decodedObject: T = decodedObjectForJSONDictionary(JSONDictionary) {
+                    objects.append(decodedObject)
+                }
+            }
+        }
+        
+        return objects
+    }
+    
+    static func objectForJSON<T: Decodable>(JSONName: String) -> T? {
+        let JSONDictionary = JSONDictionaryForName(JSONName)
+        
+        if let JSONDictionary = JSONDictionary {
+            return decodedObjectForJSONDictionary(JSONDictionary)
+        }
+        
+        return nil
+    }
+    
+    private static func decodedObjectForJSONDictionary<T: Decodable>(JSONDictionary: [String: AnyObject]) -> T? {
+        do {
+            return try T.decode(JSONDictionary)
+        } catch {}
+        
+        return nil
+    }
+    
+    private static func JSONArrayForName(JSONName: String) -> [[String: AnyObject]]? {
+        return JSONObjectForName(JSONName)
+    }
+    
+    private static func JSONDictionaryForName(JSONName: String) -> [String: AnyObject]? {
+        return JSONObjectForName(JSONName)
+    }
+    
+    private static func JSONObjectForName<T>(JSONName: String) -> T? {
+        if let path = NSBundle.mainBundle().pathForResource(JSONName, ofType: "json") {
+            if let jsonData = NSData(contentsOfFile: path) {
+                do {
+                    return try NSJSONSerialization.JSONObjectWithData(jsonData, options: .AllowFragments) as? T
+                } catch {}
+                
+                return nil
+            }
+        }
+        
+        return nil
+    }
+}
