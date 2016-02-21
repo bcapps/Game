b05e56b5c5fef2b0d643b73f25029be8d2ac76f6
twigz <aharris.1990@gmail.com>
2016-02-21 17:40:26 -0500

    Adds item

---
+++ Dark Days/Dark Days/Dark Days/Item.swift
@@ -0,0 +1,64 @@
+//
+//  Item.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+//LCKItemSlotNone,
+//LCKItemSlotHelmet,
+//LCKItemSlotChest,
+//LCKItemSlotBoots,
+//LCKItemSlotAccessory,
+//LCKItemSlotOneHand,
+//LCKItemSlotTwoHand,
+//LCKItemSlotSpell
+
+enum ItemSlot: String {
+    case None = "None"
+    case Helmet = "Helmet"
+    case Chest = "Chest"
+    case Boots = "Boots"
+    case Accessory = "Accessory"
+    case OneHand = "One Handed"
+    case TwoHand = "Two Handed"
+    
+    static func itemSlotForItemString(itemString: String) -> ItemSlot {
+        switch itemString {
+            case "helmet":
+                return .Helmet
+            case "chest":
+                return .Chest
+            case "boots":
+                return .Boots
+            case "accessory":
+                return .Accessory
+            case "onehand":
+                return .OneHand
+            case "twohand":
+                return .TwoHand
+            default:
+                return .None
+        }
+    }
+}
+
+struct Item: Decodable {
+    let name: String
+    let damage: String
+    let effects: String
+    let flavor: String
+    let itemSlot: ItemSlot
+    
+    static func decode(json: AnyObject) throws -> Item {
+        return try Item(name: json => "name",
+            damage: json => "damage",
+            effects: json => "effects",
+            flavor: json => "flavor",
+            itemSlot: ItemSlot.itemSlotForItemString(json => "itemSlot"))
+    }
+}
