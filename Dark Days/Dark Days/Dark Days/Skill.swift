26ecc68242c95eabcb27a835484706a98ce62c2a
twigz <aharris.1990@gmail.com>
2016-02-21 17:07:58 -0500

    Adds skills

---
+++ Dark Days/Dark Days/Dark Days/Skill.swift
@@ -0,0 +1,22 @@
+//
+//  Skill.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+struct Skill: Decodable {
+    let name: String
+    let explanation: String
+    let benefit: String
+    
+    static func decode(json: AnyObject) throws -> Skill {
+        return try Skill(name: json => "name",
+            explanation: json => "explanation",
+            benefit: json => "benefit")
+    }
+}
