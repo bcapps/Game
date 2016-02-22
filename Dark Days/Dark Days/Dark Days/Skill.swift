9f6edf4fa9d52c7bd0395732221285d898e997e5
twigz <aharris.1990@gmail.com>
2016-02-22 00:05:19 -0500

    Adds skill encoding

--- Dark Days/Dark Days/Dark Days/Skill.swift
+++ Dark Days/Dark Days/Dark Days/Skill.swift
@@ -10,6 +10,8 @@ import Foundation
 import Decodable
 
 struct Skill: Decodable, Nameable {
+    typealias CoderType = SkillCoder
+    
     let name: String
     let explanation: String
     let benefit: String
@@ -18,5 +20,45 @@ struct Skill: Decodable, Nameable {
         return try Skill(name: json => "name",
             explanation: json => "explanation",
             benefit: json => "benefit")
+    }
+}
+
+final class SkillCoder: NSObject, Coder {
+    typealias CodeableType = Skill
+    
+    private enum Keys: String {
+        case Name
+        case Explanation
+        case Benefit
+    }
+    
+    var value: Skill?
+    
+    init(value: Skill) {
+        self.value = value
+        super.init()
+    }
+    
+    init?(coder aDecoder: NSCoder) {
+        let rawName = aDecoder.decodeObjectForKey(Keys.Name.rawValue) as? String
+        let rawExplanation = aDecoder.decodeObjectForKey(Keys.Explanation.rawValue) as? String
+        let rawBenefit = aDecoder.decodeObjectForKey(Keys.Benefit.rawValue) as? String
+        
+        guard let name = rawName, explanation = rawExplanation, benefit = rawBenefit else {
+            value = nil
+            super.init()
+            
+            return nil
+        }
+        
+        value = Skill(name: name, explanation: explanation, benefit: benefit)
+        
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(value?.name, forKey: Keys.Name.rawValue)
+        aCoder.encodeObject(value?.explanation, forKey: Keys.Explanation.rawValue)
+        aCoder.encodeObject(value?.benefit, forKey: Keys.Benefit.rawValue)
     }
 }
