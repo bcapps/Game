7de083e2f0ebbfe14c722088d32fe3c3f89be5fd
Twigz <aharris.1990@gmail.com>
2016-02-22 10:11:01 -0500

    restructure and remove app delegate code

---
+++ Dark Days/Dark Days/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+}
+
