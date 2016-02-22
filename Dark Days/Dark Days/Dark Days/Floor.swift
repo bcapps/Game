43eb283f0ace36f9bc483763391a715df9a65107
twigz <aharris.1990@gmail.com>
2016-02-21 19:17:03 -0500

    Made floor have a coder

--- Dark Days/Dark Days/Dark Days/Floor.swift
+++ Dark Days/Dark Days/Dark Days/Floor.swift
@@ -20,3 +20,41 @@ struct Floor: Decodable, Nameable {
             towns: json => "towns")
     }
 }
+
+final class FloorCoder: NSObject, NSCoding {
+    private enum Keys: String {
+        case Name
+        case Background
+        case Towns
+    }
+
+    var floor: Floor?
+    
+    init(floor: Floor) {
+        self.floor = floor
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        let rawName = aDecoder.decodeObjectForKey(Keys.Name.rawValue) as? String
+        let rawBackground = aDecoder.decodeObjectForKey(Keys.Background.rawValue) as? String
+        let rawTowns = aDecoder.decodeObjectForKey(Keys.Towns.rawValue) as? [String]
+        
+        guard let name = rawName, background = rawBackground, towns = rawTowns else {
+            floor = nil
+            super.init()
+            
+            return nil
+        }
+        
+        floor = Floor(name: name, background: background, towns: towns)
+        
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(floor?.name, forKey: Keys.Name.rawValue)
+        aCoder.encodeObject(floor?.background, forKey: Keys.Background.rawValue)
+        aCoder.encodeObject(floor?.towns, forKey: Keys.Towns.rawValue)
+    }
+}
