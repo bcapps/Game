166f5dbe5294ee5f4f56e7f4077fd676a45b1a12
twigz <aharris.1990@gmail.com>
2016-02-21 21:51:45 -0500

    Wrote test to confirm persistence, it worked

---
+++ Dark Days/Dark Days/Dark DaysTests/InventoryPersistenceTests.swift
@@ -0,0 +1,32 @@
+//
+//  InventoryPersistenceTests.swift
+//  Dark Days
+//
+//  Created by Andrew Harrison on 2/21/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import XCTest
+
+@testable import Dark_Days
+
+class InventoryPersistenceTests: XCTestCase {
+    func testInventoryPersistence() {
+        var sut = Inventory()
+        
+        let item = Item(name: "Item", damage: "Damage", effects: "Effects", flavor: "Flavor", itemSlot: ItemSlot.Helmet)
+        
+        sut.items.append(item)
+        sut.gold = 5000
+        
+        let ic = InventoryCoder(value: sut)
+        let data = NSKeyedArchiver.archivedDataWithRootObject(ic)
+        let unarchivedIC = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? InventoryCoder
+        
+        let inventory = unarchivedIC?.value
+        let firstItem = inventory?.items.first
+        
+        XCTAssertEqual(inventory?.gold, 5000)
+        XCTAssertEqual(firstItem?.name, "Item")
+    }
+}
