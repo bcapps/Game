150937881c12bcccd75ceabc1959116cbfaf761c
twigz <aharris.1990@gmail.com>
2016-04-08 21:24:56 -0400

    Working on sending locally

--- Dark Days/Dark Days/PeerListViewController.swift
+++ Dark Days/Dark Days/PeerListViewController.swift
@@ -14,6 +14,8 @@ final class PeerListViewController: UITableViewController, LCKMultipeerEventList
     var objectToSend: Any?
     var goldToSend: Int?
     
+    private let localHeroes = HeroPersistence().allPersistedHeroes()
+    
     var peers: [MCPeerID] {
         get {
             return multipeer.connectedPeers.filter({$0.displayName != "DM"})
@@ -51,16 +53,34 @@ final class PeerListViewController: UITableViewController, LCKMultipeerEventList
     
     // MARK: UITableViewDataSource
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return peers.count
+        switch section {
+        case 0: return peers.count
+        case 1: return localHeroes.count
+        default: return 0
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell.self)) ?? UITableViewCell()
         
-        let peerID = peers[indexPath.row]
+        switch indexPath.section {
+        case 0:
+            let peerID = peers[indexPath.row]
+            
+            cell.textLabel?.attributedText = .attributedStringWithBodyAttributes(peerID.displayName)
+        case 1:
+            let hero = localHeroes[indexPath.row]
+            
+            cell.textLabel?.attributedText = .attributedStringWithBodyAttributes(hero.name)
+        default:
+            print("No handling")
+        }
         
-        cell.textLabel?.attributedText = .attributedStringWithBodyAttributes(peerID.displayName)
         cell.backgroundColor = .backgroundColor()
         cell.selectionStyle = .Gray
         
@@ -68,19 +88,34 @@ final class PeerListViewController: UITableViewController, LCKMultipeerEventList
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let peerID = peers[indexPath.row]
-        
-        if let item = objectToSend as? Item {
-            multipeer.sendItemToPeer(item, peer: peerID)
-        } else if let skill = objectToSend as? Skill {
-            multipeer.sendSkillToPeer(skill, peer: peerID)
-        } else if let spell = objectToSend as? Spell {
-            multipeer.sendSpellToPeer(spell, peer: peerID)
-        } else if let gold = goldToSend {
-            multipeer.sendGoldToPeer(gold, peer: peerID)
+        switch indexPath.section {
+        case 0:
+            let peerID = peers[indexPath.row]
+            
+            if let item = objectToSend as? Item {
+                multipeer.sendItemToPeer(item, peer: peerID)
+            } else if let skill = objectToSend as? Skill {
+                multipeer.sendSkillToPeer(skill, peer: peerID)
+            } else if let spell = objectToSend as? Spell {
+                multipeer.sendSpellToPeer(spell, peer: peerID)
+            } else if let gold = goldToSend {
+                multipeer.sendGoldToPeer(gold, peer: peerID)
+            }
+        case 1:
+            print("hi")
+        default:
+            print("No handling")
         }
         
         navigationController?.popViewControllerAnimated(true)
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Multipeer Heroes"
+        case 1: return "Local Heroes"
+        default: return nil
+        }
     }
     
     // MARK: LCKMultipeerEventListener
